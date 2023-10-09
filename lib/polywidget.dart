d045dce03a922f4262ac1291fa55c2389ccd0536
Tim Baumgart <tim.baumgart@live.com>
2023-10-09 18:49:46 +0200

    replace library polywidget with flutter_map_polywidget

--- lib/polywidget.dart
+++ lib/polywidget.dart
@@ -1,4 +1,4 @@
-library polywidget.dart;
+library flutter_map_polywidget.dart;
 
 export 'package:flutter_map_polywidget/src/polywidget.dart';
 export 'package:flutter_map_polywidget/src/tile_layer.dart';
